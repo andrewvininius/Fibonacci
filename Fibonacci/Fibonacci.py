cd4b26acbcc255afae69ed206ef56bd8a8ffbd68
Andrew Vinicius <74573141+andrewvininius@users.noreply.github.com>
2023-02-24 23:23:45 -0300

    Add files via upload

---
+++ Fibonacci/Fibonacci.py
@@ -0,0 +1,14 @@
+num = int(input("Informe um número: "))
+
+fib1 = 0
+fib2 = 1
+
+while fib2 < num:
+    temp = fib2
+    fib2 += fib1
+    fib1 = temp
+
+if fib2 == num:
+    print(num, "pertence à sequência de Fibonacci")
+else:
+    print(num, "não pertence à sequência de Fibonacci")
